fa8fc183b611ccfe89a77e73be5ac3ea2760a1d2
Jonas-Casa <jonas@impacto.ai>
2020-12-01 12:11:30 -0300

    versao teste

--- pages/restrito/login.js
+++ pages/restrito/login.js
@@ -2,19 +2,36 @@ import paginaInicial from '../paginaInicial'
 function Login() {
     return (
         <div className="container">
-            <paginaInicial />
-            <div className="row">
-                <div className="col-md-12" style={{ textAlign: "center" }}>
+            <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+            <div className="row" style={{textAlign:"center"}}>
+                <div className="col-md-12" >
                     <img className="mb-4" src="https://jonz.com.br/bzuzarte/logo_Barbara.png" width="200" height="200" />
                     <h1 className="h3 mb-3 font-weight-normal">Login</h1>
-                    <div className="input-group mb-3">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text" id="inputGroup-sizing-default">Login</span>
-                        </div>
-                        <input id="login" type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                </div>
+            </div>
+            <div className="row" >
+            <div className="col-md-3"  ></div>
+                <div className="col-md-6"  >
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Login</span>
                     </div>
+                    <input id="login" type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+                </div>
+            </div>
+            </div>
 
+            <div className="row" >
+            <div className="col-md-3"  ></div>
+                <div className="col-md-6" >
                     <div className="input-group mb-3">
                         <div className="input-group-prepend">
                             <span className="input-group-text" id="inputGroup-sizing-default">Senha</span>
@@ -24,12 +41,9 @@ function Login() {
                     <button className="btn btn-lg btn-primary btn-block" onClick={verificarLogin}  >Entrar</button>
                     <br></br>
                     <p id="mensagemErro" style={{ color: 'red', display: 'none' }}>Login e/ou senha incorretos</p>
-                    <p className="mt-5 mb-3 text-muted">© 2020</p>
-
-
+                    <p style={{textAlign:"center"}} className="mt-5 mb-3 text-muted">© 2020</p>
                 </div>
             </div>
-
         </div>
     )
 }
@@ -44,7 +58,7 @@ function verificarLogin() {
     if (meuEmail == login && minhaEntrada == senha) {
         localStorage.setItem("Token", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c');
         console.log("acesso concedido!");
-        window.location.href = "http://localhost:3000/restrito/painel";
+        window.location.href = "https://bzuzarter.vercel.app/restrito/painel";
     } else {
         let mensagemErro = document.getElementById('mensagemErro');
         mensagemErro.style.display = 'block';
--- pages/restrito/painel.js
+++ pages/restrito/painel.js
@@ -1,21 +1,35 @@
 
-function Painel(){
-    return(
-        <div onMouseOver={verificaToken}>
-            <div className="container">
-            <div className="row" id="paginaDesabilitada" style={{display:'block'}}>
-                    <div className="col-md-12">
-                        <h4 style={{textAlign:"center",}}>Pagina Restrita</h4>
+function Painel() {
+    return (
+        <div >
+            <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+            <div onMouseOver={verificaToken}>
+                <div className="container">
+                    <div className="row" id="paginaDesabilitada" style={{ display: 'block' }}>
+                        <div className="col-md-12">
+                            <h4 style={{ textAlign: "center", }}>Pagina Restrita</h4>
+                        </div>
+
                     </div>
+                    <div className="row" id="paginaRestrita" style={{ display: 'none' }}>
+                        <div className="col-md-12">
+                            <h4 style={{ textAlign: "center", }}>Painel !</h4>
 
-                </div>
-                <div className="row" id="paginaRestrita" style={{display:'none'}}>
-                    <div className="col-md-12">
-                    <h4 style={{textAlign:"center", }}>Painel !</h4>
+                            <button className="float-right" onClick={Sair} >Sair</button>
+                        </div>
+                        <br></br>
+                        <div className="col-md-12">
+                            <iframe src="https://calendar.google.com/calendar/embed?src=q3nc1glsr18a6r4lidt18l9i6uiqr7uf%40import.calendar.google.com&ctz=America%2FSao_Paulo" style={{ border: '0', width: '1920', height: '1080', frameborder: '0', scrolling: 'no' }}></iframe>
+                        </div>
 
-                    <button onClick={Sair} >Sair</button>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -25,14 +39,14 @@ function Painel(){
 
 export default Painel
 
-function verificaToken(){
+function verificaToken() {
     let meuToken = localStorage.getItem('Token');
-    if(meuToken == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'){
+    if (meuToken == 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c') {
         let paginaRestrita = document.getElementById('paginaRestrita');
         paginaRestrita.style.display = 'block';
         let paginaDesabilitada = document.getElementById('paginaDesabilitada');
         paginaDesabilitada.style.display = 'none';
-    }else{
+    } else {
         let paginaRestrita = document.getElementById('paginaRestrita');
         paginaRestrita.style.display = 'none';
         let paginaDesabilitada = document.getElementById('paginaDesabilitada');
@@ -40,7 +54,7 @@ function verificaToken(){
     }
 }
 
-function Sair(){
-    localStorage.setItem("Token","");
-    window.location.href = "http://localhost:3000/";
+function Sair() {
+    localStorage.setItem("Token", "");
+    window.location.href = "https://bzuzarter.vercel.app/";
 }
