0726e920b36f90f6f0b75fe3488cb4d43fe33c17
Jonas-Casa <jonas@impacto.ai>
2020-12-01 09:39:57 -0300

    atualizacao footer

--- pages/rodape.js
+++ pages/rodape.js
@@ -13,15 +13,12 @@ function Rodape() {
                                 <span style={{ fontFamily: 'roboto' }} className="text-muted"><p>Endereço: Av. Vicente de Carvalho, 909, Sala 1012 -</p><a href="bzuzarte@gmail.com"> E-Mail</a>  - Telefone: (21) 98897-0094 - <a href="https://www.instagram.com/bzuzartepelvica/">Instagram </a></span>
                             </div>
                         </div>
+                        <div className="col-md-12">
+                            <p style={{ fontFamily: 'roboto', textAlign: "center" }}> © Todos direitos reservados <a href="https://jonz.com.br"> Jonas</a> </p>
+                        </div>
                     </footer>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <p  style={{ fontFamily: 'roboto', textAlign: "center" }}> © Todos direitos reservados <a href="https://jonz.com.br"> Jonas</a> </p>
-                </div>
-            </div>
-
         </div>
     )
 }
