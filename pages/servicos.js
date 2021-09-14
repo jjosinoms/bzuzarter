d6c89c455a92511543da6750ba5ba3381279c0aa
Jonas-Casa <jonas@impacto.ai>
2021-09-14 14:09:41 -0300

    Teste div

--- pages/servicos.js
+++ pages/servicos.js
@@ -1,47 +1,48 @@
 function Servicos() {
 
     return (
-        <div className='row' >
-            <div className='container'  >
-                <center>
+        <div>
+            <div className='row' >
+                <div className='container'  >
+                    <center>
+                        <div className='row'>
+
+                            <div id="servicos" className='col-md-6' style={{ marginTop: '15%', backgroundColor: '#F8F8FF', backgroundImage: "url('https://imagensemoldes.com.br/wp-content/uploads/2020/05/Faixa-Preta-Sorte-PNG-1280x720.png')", backgroundSize: 'cover' }}>
+
+                                <img width='100%' src="https://jonz.com.br/bzuzarte/nova-foto-Barbara-2%20(1).png" />
+
+                            </div>
+
+                            <div className='col-md-6' style={{ marginTop: '15%' }}>
+                                <h1 style={{ marginBottom: '5%', textAlign: 'center', color: '#960202', fontFamily: 'roboto' }}>Atendimentos</h1>
+                                <h3 style={{ textAlign: 'center', color: '#960202', fontFamily: 'roboto', }}>
+
+                                    🔸Urologia (Incontinência, bexiga hiperativa, pós-prostatectomia) <br></br><br></br>
+
+                                    🔸Ginecologia (Vaginismo, vulvodínia, endometriose, dispareunia)<br></br><br></br>
+
+                                    🔸Obstetrícia (Exceto sala de parto)<br></br><br></br>
+
+                                    🔸Coloproctologia (Incontinência fecal, constipação intestinal)<br></br><br></br>
+
+                                    🔸Diástase abdominal<br></br><br></br>
+
+                                    🔸Pilates (Gestante, Mat Pilates e Aparelhos)<br></br><br></br>
+                                </h3>
+                            </div>
+
+                        </div>
+                    </center>
                     <div className='row'>
-
-                        <div id="servicos" className='col-md-6' style={{ marginTop: '15%', backgroundColor: '#F8F8FF', backgroundImage:"url('https://imagensemoldes.com.br/wp-content/uploads/2020/05/Faixa-Preta-Sorte-PNG-1280x720.png')", backgroundSize:'cover' }}>
-
-                            <img width='100%' src="https://jonz.com.br/bzuzarte/nova-foto-Barbara-2%20(1).png" />
+                        <div className='col-md-12' style={{ marginTop: '5%', marginBottom: '1%' }}>
+                            <center>
+                                <img src='https://jonz.com.br/bzuzarte/rodape.png' />
+                            </center>
 
                         </div>
-
-                        <div className='col-md-6' style={{ marginTop: '15%' }}>
-                            <h1 style={{ marginBottom: '5%', textAlign: 'center', color: '#960202', fontFamily: 'roboto' }}>Atendimentos</h1>
-                            <h3 style={{ textAlign: 'center', color: '#960202', fontFamily: 'roboto', }}>
-
-                                🔸Urologia (Incontinência, bexiga hiperativa, pós-prostatectomia) <br></br><br></br>
-
-                                🔸Ginecologia (Vaginismo, vulvodínia, endometriose, dispareunia)<br></br><br></br>
-
-                                🔸Obstetrícia (Exceto sala de parto)<br></br><br></br>
-
-                                🔸Coloproctologia (Incontinência fecal, constipação intestinal)<br></br><br></br>
-
-                                🔸Diástase abdominal<br></br><br></br>
-
-                                🔸Pilates (Gestante, Mat Pilates e Aparelhos)<br></br><br></br>
-                            </h3>
-                        </div>
-
-                    </div>
-                </center>
-                <div className='row'>
-                    <div className='col-md-12' style={{ marginTop: '5%', marginBottom: '1%' }}>
-                        <center>
-                            <img src='https://jonz.com.br/bzuzarte/rodape.png' />
-                        </center>
-
                     </div>
                 </div>
             </div>
-
         </div>
     )
 }
